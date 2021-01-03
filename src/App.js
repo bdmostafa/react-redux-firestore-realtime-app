3c0084a1a307f3b42c1ce8dabb354cbf2c4d7a28
bdmostafa <mdmostafaalmahmud@gmail.com>
2021-01-03 22:19:20 +0600

    signin and signup form with routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import NavBar from "./components/layout/NavBar";
 import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetail from "./components/projects/ProjectDetail";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
 
 function App() {
   return (
@@ -12,7 +14,8 @@ function App() {
         <Switch>
           <Route exact path='/' component={Dashboard} />
           <Route path='/project/:id' component={ProjectDetail} />
-
+          <Route path='/signin' component={SignIn}/>
+          <Route path='/signup' component={SignUp} />
         </Switch>
       </div>
     </BrowserRouter>
---
+++ src/components/auth/SignUp.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+const SignUp = () => {
+  const [newUser, setNewUser] = useState({
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: ""
+  });
+
+  const handleChange = (e) => {
+    setNewUser({...newUser, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(newUser);
+  };
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit} className="white">
+        <h3 className="grey-text text-darken-3">Sign Up</h3>
+        <div className="input-field">
+          <label htmlFor="firstName">First Name</label>
+          <input type="text" id="firstName" onChange={handleChange} />
+        </div>
+        <div className="input-field">
+          <label htmlFor="lastName">Last Name</label>
+          <input type="text" id="lastName" onChange={handleChange} />
+        </div>
+        <div className="input-field">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" onChange={handleChange} />
+        </div>
+        <div className="input-field">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" onChange={handleChange} />
+        </div>
+        <div className="input-field">
+          <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
